2127bcd7944fc4d7b07a1af7927164ddab3957c1
Krystian Wesek <kwesek@gmail.com>
2023-07-05 21:14:23 +0200

    Initial commit

---
+++ app/tests/LanguageTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class LanguageTest extends \PhpUnit\Framework\TestCase
+{
+  public function testItWorks()
+  {
+    $this->assertTrue(true);
+  }
+}
---
+++ info.md
@@ -0,0 +1,1 @@
+https://github.com/GaryClarke/docker-php
